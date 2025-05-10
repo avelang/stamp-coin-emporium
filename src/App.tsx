634ba6278d8566d390fb7ab2f6e28982f500a88b
BONTERRATECH\c-abey.velangadan <c-abey.velangadan@bonterratech.com>
2025-05-10 16:38:12 +0530

    Fix router configuration for GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -18,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/stamp-coin-emporium">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/browse" element={<BrowsePage />} />
